f240bf66c67ef3e9ccd626501b357a774718160a
KhokhlovArt <KhokhlovArt@gmail.com>
2018-04-05 17:15:40 +0300

    test v 1.5
    Тестирование библиотеки версии 1.5

--- app/src/main/java/com/mks/gaidlibtest/MainActivity.java
+++ app/src/main/java/com/mks/gaidlibtest/MainActivity.java
@@ -8,11 +8,14 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.advertising_id_service.appclick.googleadvertisingidservice.GoogleAdvertisingIdGetter;
+import com.advertising_id_service.appclick.googleadvertisingidservice.IGoogleAdvertisingIdGetter;
+import com.advertising_id_service.appclick.googleadvertisingidservice.PublisherID.PublisherIDMask;
 import com.google.android.gms.ads.identifier.AdvertisingIdClient;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 
 import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,80 +35,95 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn_show_original_gaid = (Button)   findViewById(R.id.btn_show_original_gaid);
-        Button btn_show_fake_gaid     = (Button)   findViewById(R.id.btn_show_fake_gaid);
 
-        final TextView lbl_original_gaid = (TextView) findViewById(R.id.lbl_original_gaid);
-        final TextView lbl_fake_gaid     = (TextView) findViewById(R.id.lbl_fake_gaid);
-
-        btn_show_original_gaid.setOnClickListener(new View.OnClickListener() {
+        Button btn1  = (Button) findViewById(R.id.btn1);
+        final TextView lbl_1 = (TextView)  findViewById(R.id.lbl_1);
+        Button btn2  = (Button) findViewById(R.id.btn2);
+        final TextView lbl_2 = (TextView)  findViewById(R.id.lbl_2);
+        Button btn3  = (Button) findViewById(R.id.btn3);
+        final TextView lbl_3 = (TextView)  findViewById(R.id.lbl_3);
+        Button btn4  = (Button) findViewById(R.id.btn4);
+        final TextView lbl_4 = (TextView)  findViewById(R.id.lbl_4);
+        Button btn5  = (Button) findViewById(R.id.btn5);
+        final TextView lbl_5 = (TextView)  findViewById(R.id.lbl_5);
+        Button btn6  = (Button) findViewById(R.id.btn6);
+        final TextView lbl_6 = (TextView)  findViewById(R.id.lbl_6);
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 AsyncTask.execute(new Runnable() {
                     @Override
                     public void run() {
-
-                        String realGAID = "";
-                        AdvertisingIdClient.Info adInfo = null;
+                        String data = null;
                         try {
-                            adInfo = AdvertisingIdClient.getAdvertisingIdInfo(getApplicationContext());
-                        } catch (IOException e) {
-                            setTextInMainGIThred(R.id.lbl_original_gaid, "Unrecoverable error connecting to Google Play services");
-                            e.printStackTrace();
+                            data = new GoogleAdvertisingIdGetter().getOriginalID(getApplicationContext());
                         } catch (GooglePlayServicesNotAvailableException e) {
-                            setTextInMainGIThred(R.id.lbl_original_gaid, "Google Play services is not available entirely");
+                            e.printStackTrace();
+                        } catch (IOException e) {
                             e.printStackTrace();
                         } catch (GooglePlayServicesRepairableException e) {
-                            setTextInMainGIThred(R.id.lbl_original_gaid, e.getMessage());
                             e.printStackTrace();
                         }
-                        if(adInfo != null) {
-                            String adInfoId = adInfo.getId();
-                            if(adInfoId != null) {
-                                realGAID = adInfoId;
-                                setTextInMainGIThred(R.id.lbl_original_gaid, realGAID);
-                            }
-                            else
-                            {
-                                setTextInMainGIThred(R.id.lbl_original_gaid, "original gaid is NULL");
-                            }
-                        }
+                        setTextInMainGIThred(R.id.lbl_1, "" + data);
                     }
                 });
             }
         });
 
-        btn_show_fake_gaid.setOnClickListener(new View.OnClickListener() {
+
+
+        btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 AsyncTask.execute(new Runnable() {
                     @Override
-                    public void run() {
-
+                    public void run () {
+                        String data = null;
                         try {
-
-                            final String id = new GoogleAdvertisingIdGetter().getFakeGaid( getApplicationContext(), getApplicationContext().getPackageName(), "");
-                            setTextInMainGIThred(R.id.lbl_fake_gaid, id);
-                        } catch (IOException e) {
-                            // Unrecoverable error connecting to Google Play services (e.g.,
-                            // the old version of the service doesn't support getting AdvertisingId).
-                            setTextInMainGIThred(R.id.lbl_fake_gaid, "Unrecoverable error connecting to Google Play services");
+                            data = new GoogleAdvertisingIdGetter().getFakeGaid(getApplicationContext());
                         } catch (GooglePlayServicesNotAvailableException e) {
-                            // Google Play services is not available entirely.
-                            setTextInMainGIThred(R.id.lbl_fake_gaid, "Google Play services is not available entirely");
-                        } catch (IllegalStateException e) {
-                            setTextInMainGIThred(R.id.lbl_fake_gaid, e.getMessage());
+                            e.printStackTrace();
+                        } catch (IOException e) {
                             e.printStackTrace();
                         } catch (GooglePlayServicesRepairableException e) {
-                            setTextInMainGIThred(R.id.lbl_fake_gaid, e.getMessage());
                             e.printStackTrace();
                         }
+                        setTextInMainGIThred(R.id.lbl_2, "" + data);
                     }
                 });
             }
         });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String data = null;
+                data = new GoogleAdvertisingIdGetter().generateGUID(IGoogleAdvertisingIdGetter.GenerateIDType.DEFAULT, getApplicationContext());
+                lbl_3.setText(""+data);
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String data = null;
+                data = new GoogleAdvertisingIdGetter().generateGUID(IGoogleAdvertisingIdGetter.GenerateIDType.RANDOM, getApplicationContext());
+                lbl_4.setText(""+data);
+            }
+        });
 
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                List<String> data = new GoogleAdvertisingIdGetter().getFilePublisherIDs(getApplicationContext(), new PublisherIDMask("apk,appclick", "_", ".zip,.apk"));
+                lbl_5.setText(""+data);
+            }
+        });
+
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String data = new GoogleAdvertisingIdGetter().getInnerPublisherIDs(getApplicationContext(), "key_inner");
+                lbl_6.setText(""+data);
+            }
+        });
     }
 }
